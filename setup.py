63af4e272a56083a1099d5622f60003ff79b705d
Ravi Salunkhe <salunkhe.ravi@ymail.com>
2019-11-04 17:50:39 +0530

    changed version to 1.0.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='noompy',
-    version='1.0.0',
+    version='1.0.1',
     packages=['api'],
     url='https://github.com/salunkhe-ravi/noompy',
     license='MIT',
